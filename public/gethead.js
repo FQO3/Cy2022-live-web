2c344256ec63ade093d363273ccbe354e81aa77a
FQO3 <111474347+FQO3@users.noreply.github.com>
2024-01-17 19:03:22 +0800

    v1.5.3

--- public/gethead.js
+++ public/gethead.js
@@ -1,14 +1,14 @@
 fetch("http://cyxsh.top:1308/head.html")
   .then((response) => {
     if (response.ok) {
-      console.log(response.json);
+      // console.log(response.json);
       return response.text();
     }
     throw new Error("Network response was not ok.");
   })
   .then((data) => {
     // 处理获取到的数据
-    console.log(data);
+    // console.log(data);
     var element=document.getElementById("header");
     element.innerHTML=data;
   });
