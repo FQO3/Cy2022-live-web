0a7c41bcf812b5be48fbf81b783111cf05c36d39
FQO3 <111474347+FQO3@users.noreply.github.com>
2024-03-06 22:18:20 +0800

    v3.0.3

--- www.js
+++ www.js
@@ -69,9 +69,7 @@ app.post("/comments", (req, res) => {
 app.post("/admin/adm.html", (req, res) => {
   const currentTime = new Date();
   today = String(currentTime.getMonth() + 1) + String(currentTime.getDate());
-  console.log(today);
   pass = req.query.pass;
-  console.log(pass);
   if (pass == today) {
     res.status(200).send("viewers.html");
   }
